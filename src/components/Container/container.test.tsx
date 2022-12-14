31430784a89385a4bf5c10e52c2aebcfc729a2bb
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-14 19:47:17 -0300

    test: add position test

--- src/components/Container/container.test.tsx
+++ src/components/Container/container.test.tsx
@@ -17,7 +17,8 @@ describe("<Container/>", () => {
     expect(parent).toHaveStyle({
       width: "100%",
       margin: "0 auto",
-      padding: "0 1rem"
+      padding: "0 1rem",
+      position: "relative"
     });
 
     expect(container).toMatchSnapshot();
