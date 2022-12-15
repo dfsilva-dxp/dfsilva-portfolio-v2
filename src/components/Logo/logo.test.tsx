9afad3aee67d6f2f519405a62f281476539ccb68
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-15 14:41:41 -0300

    test: add logo tests

---
+++ src/components/Logo/logo.test.tsx
@@ -0,0 +1,18 @@
+import { screen } from "@testing-library/dom";
+import { renderWithTheme } from "../../utils/tests/renderWithTheme";
+
+import Logo from ".";
+
+describe("<Logo/>", () => {
+  it("should render currectly logo", () => {
+    const { container } = renderWithTheme(<Logo />);
+
+    expect(screen.getByLabelText(/daniel silva/i).parentElement).toHaveStyle({
+      color: "#E1E1E6",
+      position: "relative",
+      "z-index": 10
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
