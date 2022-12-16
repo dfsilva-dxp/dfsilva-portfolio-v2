3237186a6213e091adf31e0cd088b456018ef925
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-16 11:53:29 -0300

    feat: add navbar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as Container } from "./Container";
 export { default as Glitch } from "./Glitch";
 export { default as Link } from "./Link";
 export { default as Logo } from "./Logo";
+export { default as Navbar } from "./Navbar";
 export { default as Section } from "./Section";
