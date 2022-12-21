f6f9180ae268a21e1d49c87c44d07ef39bd4423f
Daniel Silva <dfsilva.dxp@gmail.com>
2022-12-21 09:45:37 -0300

    feat: add logo particles component

--- src/components/LogoParticles/index.tsx
+++ src/components/LogoParticles/index.tsx
@@ -1,47 +1,26 @@
-import useWindowSize from "@rooks/use-window-size";
-import ParticleImage, {
-  ParticleOptions,
-  Vector,
-  forces,
-  ParticleForce
-} from "react-particle-image";
+import ParticleImage from "react-particle-image";
+import { useWindowSize } from "usehooks-ts";
+
+import { motionForce, particleOptions } from "../../utils/particles";
 
 import logo from "../../assets/logo.svg";
 
-const particleOptions: ParticleOptions = {
-  filter: ({ x, y, image }) => {
-    const pixel = image.get(x, y);
-    return pixel.b > 5;
-  },
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  color: ({ x, y, image }) => "#020015",
-  radius: () => Math.random() * 0.5 + 0.5,
-  mass: () => 10,
-  friction: () => 0.35,
-  initialPosition: ({ canvasDimensions }) => {
-    return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
-  }
-};
-
-const motionForce = (x: number, y: number): ParticleForce => {
-  return forces.disturbance(x, y, 5);
-};
-
 const LogoParticles = () => {
-  const { innerWidth, innerHeight } = useWindowSize();
+  const { width, height } = useWindowSize();
 
   return (
     <ParticleImage
+      aria-label="Logo Particles"
       src={logo}
-      width={Number(innerWidth)}
-      height={Number(innerHeight)}
+      width={width}
+      height={height - 120}
       scale={1}
       entropy={0.15}
       maxParticles={30000}
       particleOptions={particleOptions}
       mouseMoveForce={motionForce}
       touchMoveForce={motionForce}
-      backgroundColor="#CB7F65"
+      backgroundColor="transparent"
     />
   );
 };
